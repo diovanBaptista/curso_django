782af19911d9591b5d6cbe3b75132e405fba47c4
diovan baptista <diovantrab@gmail.com>
2023-01-27 10:43:23 -0300

    configuração de urla e link

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home) , # home
-    path('recipes/<int:id>/', views.recipes) , # home
+    path('', views.home, name="recipes-home") , # home
+    path('recipes/<int:id>/', views.recipes, name="recipes-recipe") , # home
 ]
